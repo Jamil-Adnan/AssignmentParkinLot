ecd1c587983a90a83ee885d921faf9e8b04cde97
ArataKido <shahzod20030905@gmail.com>
2024-11-06 13:42:39 +0100

    added method for adding vehicles

--- ParkinLot/Parking.cs
+++ ParkinLot/Parking.cs
@@ -8,33 +8,58 @@ namespace ParkinLot
 {
     internal class Parking
     {
-        static int totalParking = 25;
-        static private List<Transport> parkingLot;
-        public Parking()
+        public int totalParking;
+        public  List<List<Transport>> parkingLot;
+        public Parking(int parkingAmount)
         {
-            parkingLot = new List<Transport>();
+            totalParking = parkingAmount;
+            parkingLot = new List<List<Transport>>();
         }
-        static double CheckParking()
+        public double CheckParking()
         {
             double occupiedParking = 0;
-            foreach (Transport transport in parkingLot)
+            foreach (List<Transport> parking in parkingLot)
             {
-                occupiedParking += transport.Size;                     
+                foreach (Transport transport  in parking){
+                    if (transport is null){
+
+                    }                    
+                }
             }
             return totalParking - occupiedParking;
         }
-        public bool ParkVehicle(Transport vehicle) 
+
+        public List<int> FindEmptyParkingSlots()
         {
-            if (CheckParking() > vehicle.Size)
+        List<int> emptySlots = new List<int>();
+        for (int i = 0; i < parkingLot.Count; i++)
+        {
+            if (parkingLot[i].Count == 0)
             {
-                parkingLot.Add(vehicle);
-                Console.WriteLine("Fordonet går att parkera på parkering.");
-                return true;
+                emptySlots.Add(i);
             }
-           
-            return false;
-           
-            
+        }
+        return emptySlots;
+        }
+        
+        public bool ParkVehicle(Transport vehicle)
+        {
+        List<int> emptySlots = FindEmptyParkingSlots();
+        if (emptySlots.Count > 0)
+        {
+            int slotIndex = emptySlots[0]; // Get the first empty slot
+            parkingLot[slotIndex].Add(vehicle);
+            Console.WriteLine($"Fordonet har parkerats på plats {slotIndex + 1}.");
+            return true;
+        }
+        Console.WriteLine("Det finns inte tillräckligt med plats för att parkera fordonet.");
+        return false;
+        }
+
+        public void createParkingLots(){
+            for (int i = 0; i < totalParking; i++ ){
+                parkingLot.Add(new List<Transport>());
+            };
         }
     } 
 
