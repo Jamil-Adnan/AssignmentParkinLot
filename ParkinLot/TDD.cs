c0f489b038452dc6a01e04f8db8ba6fe3fd937c2
Adnan Jamil <jamil.adnan@live.com>
2024-11-15 00:56:19 +0100

    Innitiating a new branch named TDD. Will use this branch for tests. Now updating it with the first test. used 3 different methods and all went as planned.

---
+++ ParkinLot/TDD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkinLot
+{
+    public class TDD
+    {        
+        public static double CalculateTotalAmount(int desiredTime, int checkOutTime) 
+        {
+            desiredTime = Math.Abs(desiredTime);
+            checkOutTime = Math.Abs(checkOutTime);
+            int checkInTime = 0;
+            double pricePerSecond = 1.5;
+            int expectedCheckOutTime = checkInTime + desiredTime;
+            double totalAmount = desiredTime * pricePerSecond;            
+            int timeSpanDiff = checkOutTime - expectedCheckOutTime;
+            int fine = 500;
+            if (timeSpanDiff > 0)
+            {
+                return totalAmount += fine;                
+            }            
+            return 0;            
+        }        
+    }
+}
