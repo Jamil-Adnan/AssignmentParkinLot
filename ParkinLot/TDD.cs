4078d377a221c9b075008a0270dde619f49913f9
Adnan Jamil <jamil.adnan@live.com>
2024-11-19 09:49:29 +0100

    SORRY!!! deleted the old one (I have done something very stupid and the whole branch stopped working. Deleted the old one and creating a new.)

---
+++ ParkinLot/TDD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkinLot
+{
+    public class TDD
+    {
+        public static double CalculateTotalAmount(int desiredTime, int checkOutTime)
+        {
+            desiredTime = Math.Abs(desiredTime);
+            checkOutTime = Math.Abs(checkOutTime);
+            int checkInTime = 0;
+            double pricePerSecond = 1.5;
+            int expectedCheckOutTime = checkInTime + desiredTime;
+            double totalAmount = desiredTime * pricePerSecond;
+            int timeSpanDiff = checkOutTime - expectedCheckOutTime;
+            int fine = 500;
+            if (timeSpanDiff > 0)
+            {
+                return totalAmount += fine;
+            }
+            return 0;
+        }
+       /*
+        public void CalculateAmountWithLargeExceededTime()
+        {
+            double expected = 1500;
+            double expected = 650;
+            var result = ParkinLot.TDD.CalculateTotalAmount(100, 510);
+            Assert.AreEqual(expected, result);
+        }
+       */
+    }
+}
